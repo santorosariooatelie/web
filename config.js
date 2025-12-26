c23becaf5fe6f14565d6e2ee648d25162cfefdcc
santorosariooatelie <santorosariooatelie@gmail.com>
2025-12-26 18:32:25 -0300

    config

---
+++ config.js
@@ -0,0 +1,11 @@
+// CONFIGURAÇÕES SENSÍVEIS - NÃO COMPARTILHE ESTE ARQUIVO
+// Este arquivo contém dados que você não quer que apareçam no repositório público
+
+// Seu número WhatsApp (incluir código do país + DDD + número, sem espaços ou caracteres especiais)
+// Exemplo: 5511987654321 (55=Brasil, 11=São Paulo, 987654321=seu número)
+const CONFIG = {
+    WHATSAPP_NUMBER: '5511999999999',
+    
+    // URL da planilha Google Sheets com seus produtos
+    GOOGLE_SHEET_URL: 'https://opensheet.elk.sh/1rn0VFaxpFQX7gaUQnKnXdFC_rm0hGDE9hdPnRDfAX1Q/1'
+};
